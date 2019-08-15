4c2022f68d7fc5907ebe363b3031f2899c9aab8e
deathclawhunter <front.zy.dk.002@gmail.com>
2019-08-15 12:02:38 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -11,8 +11,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form('username')
-        password = request.form('password')
+        username = request.form['username']
+        password = request.form['password']
         db = get_db()
         error = None
         
@@ -27,3 +27,21 @@ def register():
             return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute('SELECT * FROM user WHERE username=?', (username,)).fetchone()
+        if user is None: error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password): error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
