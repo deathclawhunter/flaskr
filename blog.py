0bb632b3bf5a46a77c9b208445337ca3fd587e1c
deathclawhunter <front.zy.dk.002@gmail.com>
2019-08-15 16:26:36 -0700

    Update blog.py

--- blog.py
+++ blog.py
@@ -35,7 +35,7 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
         flash(error)
-        return render_template('blog/create.html')
+    return render_template('blog/create.html')
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -64,7 +64,7 @@ def update(id):
             db.commit()
             return redirect(url_for('blog.index'))
         flash(error)
-        return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
