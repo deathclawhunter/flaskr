dccabccc85a09dcd45f5792f2814e044dfb83800
deathclawhunter <front.zy.dk.002@gmail.com>
2019-08-15 14:23:33 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 # export FLASK_APP=flaskr && export FLASK_ENV=development && flask init-db
 killall flask
-export FLASK_APP=flaskr && export FLASK_ENV=development && flask run &
+export FLASK_APP=flaskr && export FLASK_ENV=development && flask run -h 0.0.0.0 &
 # flask run
