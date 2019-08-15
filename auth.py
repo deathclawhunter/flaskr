fd20d6cd40161663def6ba0f1845c8137c681847
deathclawhunter <front.zy.dk.002@gmail.com>
2019-08-15 14:00:32 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,8 @@
 import functools
 
-from flask import { Blueprint, flash, g, redirect, render_template, request, session, url_for }
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask.db import get_db
 
